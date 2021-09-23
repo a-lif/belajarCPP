9e13860ce4d85c64c3e85d47f1718c2f7be0e04d
Alif Nurcahyo <63556067+rw4arch@users.noreply.github.com>
2021-09-23 17:21:38 +0700

    Add files via upload

---
+++ 06_Printing/printing.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+
+int main()
+{
+	std::cout << "Hello World\n";
+	std::cout << "Ini adalah awal.";
+	std::cout << "Ini adalah tengah.";
+	std::cout << " Ini adalah akhir" << std::endl;
+	return 0;
+}

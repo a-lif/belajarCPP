7284d277ed54b5d19712fce65d6632c73c46df53
Alif Nurcahyo <63556067+a-lif@users.noreply.github.com>
2021-09-25 12:59:57 +0700

    Add files via upload

---
+++ 08_Deklarasi/deklarasi.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	int a;
+	a = 3000;
+	cout << a << endl;
+
+	int b;
+	cout << "masukkan sebuah nilai: ";
+	cin >> b;
+	cout << "nilai yang anda masukkan adalah ";
+	cout << b << endl;
+
+	return 0;
+}

5b216b73e1a930d92dd0d0087cd9a0a9e781ed03
Alif Nurcahyo <63556067+a-lif@users.noreply.github.com>
2021-09-30 18:29:31 +0700

    Add files via upload

---
+++ 13_If_Statement/ifstatement.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a;
+
+	cout << "masukkan angka: ";
+	cin >> a;
+
+	if (a == 15){
+		cout << "nilai adalah kelipatan 5";
+	}
+
+	cin.get();
+	return 0;
+}

bab3df7c0358e6a50bafd417f7871ffce795961c
Alif Nurcahyo <63556067+a-lif@users.noreply.github.com>
2021-10-05 15:26:22 +0700

    Add files via upload

---
+++ 18_Increment_dan_Decrement/indec.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// increment dan decrement
+	// preincrement dan postincrement
+	// predecrement dan postdecrement
+
+	int a = 5;
+	int b = 5;
+
+	// postincrement
+	cout << a << endl;
+	cout << a++ << endl;
+	cout << a << endl << endl;
+
+	// preincrement
+	cout << b << endl;
+	cout << ++b << endl;
+	cout << b << endl;
+
+	cin.get();
+	return 0;
+}
